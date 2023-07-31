6ae029f64e221668fcfefd9f434ce64820b9ca7f
Marko <mare.sampbn@gmail.com>
2023-07-31 23:18:13 +0200

    dev

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment  = {
+  firebase: {
+    projectId: 'adreward-8b3a6',
+    appId: '1:429609241715:web:e742224f3165fed5e7ee06',
+    storageBucket: 'adreward-8b3a6.appspot.com',
+    apiKey: 'AIzaSyCAJjSyyHzUq7rtu6q7jbz5ioPRI5HzmDw',
+    authDomain: 'adreward-8b3a6.firebaseapp.com',
+    messagingSenderId: '429609241715',
+    measurementId: 'G-57GRV3FBXG',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCAJjSyyHzUq7rtu6q7jbz5ioPRI5HzmDw",
+    authDomain: "adreward-8b3a6.firebaseapp.com",
+    projectId: "adreward-8b3a6",
+    storageBucket: "adreward-8b3a6.appspot.com",
+    messagingSenderId: "429609241715",
+    appId: "1:429609241715:web:e742224f3165fed5e7ee06",
+    measurementId: "G-57GRV3FBXG"
+  }
+}
